0866ea5b79ebcbc1661f1eaf1e940c3739c5e5d9
Brian Neel <brian@gitlab.com>
2017-07-05 19:51:59 -0400

    Add final Rails Security Guide checks

---
+++ lib/rubocop/cop/gitlab-security/sql_injection.rb
@@ -0,0 +1,39 @@
+module RuboCop
+  module Cop
+    module GitlabSecurity
+      # Check for use of where("name = '#{params[:name]}'")
+      #
+      # Passing user input to where() without parameterization can result in SQL Injection
+      # 
+      # @example
+      #
+      #   # bad
+      #   u = User.where("name = '#{params[:name]}'")
+      # 
+      #   # good (parameters)
+      #   u = User.where("name = ? AND id = ?", params[:name], params[:id])
+      #   u = User.where(name: params[:name], id: params[:id])
+      #
+      class SqlInjection < RuboCop::Cop::Cop
+        MSG = 'Parameterize all user-input passed to where(), do not directly embed user input in SQL queries'
+   
+        def_node_matcher :where_user_input?, <<-PATTERN
+          (send _ :where ...)
+        PATTERN
+
+        def_node_matcher :string_var_string?, <<-PATTERN
+          (dstr (str ...) (begin ...) (str ...) ...)
+        PATTERN
+
+        def on_send(node)
+          return unless where_user_input?(node)
+          return unless node.method_args.any? { |e| string_var_string?(e) }
+
+          puts "#{node}"
+
+          add_offense(node, :selector)
+        end
+      end
+    end
+  end
+end
